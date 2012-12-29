47a98efe5fe8e60dec9f944f6b7001e1587685dd
okamurayasuyuki <haradashinya@gmail.com>
2012-12-29 14:49:57 +0900

    add layout.css

--- app.py
+++ app.py
@@ -1,11 +1,27 @@
 from flask import Flask
 from flask import render_template
+from flask import request,url_for
 app = Flask(__name__)
 
 
 @app.route("/")
 def index():
+    # show all blog page
     return render_template("index.html")
+
+
+@app.route("/posts",methods=["GET","POST"])
+def post():
+    if request.method == "POST":
+        print "none"
+        return None
+    elif request.method == "GET":
+        print "ffff"
+        # render create post page.
+        return render_template("new_post.html")
+
+
+
 
 
 
