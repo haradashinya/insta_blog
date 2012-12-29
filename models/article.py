9909e2deabd5888ab4a03732af01f9f4d0715fef
okamurayasuyuki <haradashinya@gmail.com>
2012-12-29 17:50:50 +0900

    save

--- models/article.py
+++ models/article.py
@@ -4,15 +4,25 @@ r = redis.StrictRedis(host="localhost",port=6379,db=0)
 
 class Article(object):
     def __init__(self):
+        r.flushall()
         print("init")
 
     def time(self):
         return datetime.now().strftime("%Y %m/%d %H:%m")
 
     def create(self,text):
-        print("called create")
-        print(text)
+        id = r.incr("article_id")
+        ns = "articles:%s" % id
+        print(ns)
+        r.hmset(ns,{"body":text,"created_at":self.time()})
+
+
+    def update(self,id):
         pass
+
+    def destroy(self,id):
+        pass
+
 
 
 
